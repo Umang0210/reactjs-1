ae85efa92917a9ae41306d783b6ca5f8906fbb76
Umang <umanggarg0210@gmail.com>
2026-03-13 20:35:39 +0530

    index.html

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import Register from "./components/Register";
+import Cart from "./components/Cart";
+import Orders from "./components/Orders";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route index element={<Content />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="orders" element={<Orders />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="logout" element={<Logout />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+export default App;
---
+++ src/components/Content.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+function Content() {
+  const [products, setProducts] = useState([]);
+  const API_URL = import.meta.env.VITE_API_URL;
+  const fetchProducts = async () => {
+    const url = `${API_URL}/store`;
+    const res = await axios.get(url);
+    setProducts(res.data);
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      {products &&
+        products.map((product) => (
+          <div key={product._id}>
+            <img
+              src={`${API_URL}${product.imageUrl}`}
+              width={300}
+              alt=""
+            />
+            <h3>{product.name}</h3>
+            <p>{product.desc}</p>
+            <h4>{product.price}</h4>
+            <button>Add to Cart</button>
+          </div>
+        ))}
+    </div>
+  );
+}
+
+export default Content;
